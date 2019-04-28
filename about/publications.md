b57c59655f9e032c620dd869acbe15a27c11883b
Marcelo Brandalero <b.marcelo@gmail.com>
2019-04-28 18:59:43 +0200

    Include publications list + publications parser

---
+++ about/publications.md
@@ -0,0 +1,34 @@
+---
+layout: page
+title: Publications
+subtitle: Marcelo Brandalero
+---
+
+
+# Conference Publications
+
+**M Brandalero**, GM Malfatti, GF Oliveira, LA Da, LR Gonçalves, BC Da, L Carro, ACS Beck: *Efficient Local Memory Support for Approximate Computing*. In: 2018 VIII Brazilian Symposium on Computing Systems Engineering (SBESC), 2019.
+
+GF Oliveira, LR Gonçalves, **M Brandalero**, ACS Beck, L Carro: *Employing classification-based algorithms for general-purpose approximate computing*. In: Proceedings of the 55th Annual Design Automation Conference, 2018.
+
+PH Exenberger, AL Sartor, **M Brandalero**, TT Jost, S Wong, L Carro, AC Beck: *A Low-Cost BRAM-Based Function Reuse for Configurable Soft-Core Processors in FPGAs*. In: Applied Reconfigurable Computing. Architectures, Tools, and Applications: 14th International Symposium, ARC 2018, Santorini, Greece, May 2-4, 2018, Proceedings 14, 2018.
+
+**M Brandalero**, L Carro, ACS Beck, M Shafique: *Approximate on-the-fly coarse-grained reconfigurable acceleration for general-purpose applications*. In: 2018 55th ACM/ESDA/IEEE Design Automation Conference (DAC), 2018.
+
+**M Brandalero**, ACS Beck: *A Mechanism for energy-efficient reuse of decoding and scheduling of x86 instruction streams*. In: Proceedings of the Conference on Design, Automation & Test in Europe, 2017.
+
+LA Silveira, **M Brandalero**, JD Souza, ACS Beck: *The Potential of Accelerating Image-Processing Applications by using Approximate Function Reuse*. In: 2016 VI Brazilian Symposium on Computing Systems Engineering (SBESC), 2016.
+
+**M Brandalero**, ACS Beck: *Potential of using a reconfigurable system on a superscalar core for ILP improvements*. In: 2014 Brazilian Symposium on Computing Systems Engineering, 2014.
+
+
+# Journal Publications
+
+**M Brandalero**, LA Silveira, JD Souza, ACS Beck: *Accelerating error-tolerant applications with approximate function reuse*. Science of Computer Programming, 2018.
+
+PHE Becker, AL Sartor, **M Brandalero**, ACS Beck: *BRAM-based function reuse for multi-core architectures in FPGAs*. Microprocessors and Microsystems, 2018.
+
+**M Brandalero**, ACS Beck: *Potential analysis of a superscalar core employing a reconfigurable array for improving instruction-level parallelism*. Design Automation for Embedded Systems, 2016.
+
+FM Capella, **M Brandalero**, L Carro, AC Beck: *A multiple-ISA reconfigurable architecture*. Design Automation for Embedded Systems, 2015.
+
---
+++ publications_parser.py
@@ -0,0 +1,88 @@
+#!/bin/python3
+
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+
+import sys
+
+yaml_header = \
+    "---\n" + \
+    "layout: page\n" + \
+    "title: Publications\n" + \
+    "subtitle: Marcelo Brandalero\n" + \
+    "---\n\n"
+
+def customize_parser(record):
+
+    # 0) Convert to Unicode
+    record = bibtexparser.customization.convert_to_unicode(record)
+    
+    # 1) Fix names
+    # 1.1) Convert author names field from str into a list of names
+    record = bibtexparser.customization.author(record)
+    #print("1.1) " + str(record["author"]))
+    
+    # 1.2) Take each name in the list and format, adding to a new name string
+    formatted_names = ""
+    for author in record["author"]:
+        formatted_name = ""
+        author_names = bibtexparser.customization.splitname(author)
+
+        for first_name in author_names["first"]:
+            formatted_name += first_name[0]
+        formatted_name += " " + author_names["last"][0]
+        # 1.3) Make my name bold
+        if formatted_name == "M Brandalero":
+            formatted_name = "**" + formatted_name + "**"
+
+        formatted_names += formatted_name + ", "
+        
+    record["author"] = formatted_names[:-2]
+
+    #print("1.3) " + str(record["author"]))
+          
+    return record
+
+parser = BibTexParser(customization = customize_parser)
+
+def main():
+  
+    with open('publications.bib') as bibtex_file:
+        bibtex_db = bibtexparser.load(bibtex_file, parser)
+    bibtex_db.entries.sort(key = lambda x : -int(x["year"]))
+    
+    print(yaml_header)
+        
+    # Conference Publications
+    print("# Conference Publications\n")
+    for entry in bibtex_db.entries:
+        if entry["ENTRYTYPE"] == "inproceedings":
+
+            print(entry["author"], end = ": ")
+
+            print("*" + entry["title"] + "*", end = ". ")
+           
+            print("In: " + entry["booktitle"], end = ", ")
+
+            print(entry["year"], end = ".\n\n")
+
+    print("")
+    # Journal Publications
+    print("# Journal Publications\n")
+        
+    for entry in bibtex_db.entries:
+        if entry["ENTRYTYPE"] == "article":
+
+            print(entry["author"], end = ": ")
+
+            print("*" + entry["title"] + "*", end = ". ")
+           
+            print(entry["journal"], end = ", ")
+
+            print(entry["year"], end = ".\n\n")
+            
+if __name__ == "__main__":
+    sys_stdout = sys.stdout
+    sys.stdout = open("about/publications.md", "w")
+    main()
+    sys.stdout = sys_stdout
